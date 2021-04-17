fb1b85dbe1b05768cd0dc60d364c3a03fc95c843
smt-ofa <stephen.mehaut@attestationlegale.fr>
2021-04-18 01:13:32 +0200

    feat(1.0.0): try dotenv

--- index.js
+++ index.js
@@ -43,6 +43,15 @@ const core = require('@actions/core');
    // Extract archive
 // }
 
+
+
+const loadDotenvFile = () => {
+   const fs = require('fs')
+   const dotenv = require('dotenv')
+   return dotenv.parse(fs.readFileSync('.env'))
+
+};
+
 const inputs = () => {
    return {
       // The repository to fetch
@@ -75,6 +84,7 @@ async function run() {
     const settings = inputs();
     core.info(`Waiting ${ms} milliseconds ...`);
     core.debug(settings);
+    core.debug(loadDotenvFile());
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
