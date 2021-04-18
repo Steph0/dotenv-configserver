27eaedee82a5ba363cb0bccea2bd0daafc4f6266
smt-ofa <stephen.mehaut@attestationlegale.fr>
2021-04-19 00:27:59 +0200

    feat(1.0.0): download and extract remote archive repository

--- index.js
+++ index.js
@@ -1,70 +1,168 @@
 const core = require('@actions/core');
-
+const github = require('@actions/github');
+const io = require('@actions/io');
+const tc = require('@actions/tool-cache');
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+const {v4: uuidv4} = require('uuid');
 
 // function downloadConfiguration() {
 
-   // Build repo url
-   // core.info(
-   //    `Syncing repository: ${settings.repositoryOwner}/${settings.repositoryName}`
-   //  );
-   //  const repositoryUrl = urlHelper.getFetchUrl(settings);
+// Build repo url
+// core.info(
+//    `Syncing repository: ${settings.repositoryOwner}/${settings.repositoryName}`
+//  );
+//  const repositoryUrl = urlHelper.getFetchUrl(settings);
 
-   // Define 'ref'
-   // result.ref = core.getInput('ref')
-   // if (!result.ref) {
-   //   if (isWorkflowRepository) {
-   //     result.ref = github.context.ref
-   //     result.commit = github.context.sha
- 
-   //     // Some events have an unqualifed ref. For example when a PR is merged (pull_request closed event),
-   //     // the ref is unqualifed like "main" instead of "refs/heads/main".
-   //     if (result.commit && result.ref && !result.ref.startsWith('refs/')) {
-   //       result.ref = `refs/heads/${result.ref}`
-   //     }
-   //   }
-   // }
-   // // SHA?
-   // else if (result.ref.match(/^[0-9a-fA-F]{40}$/)) {
-   //   result.commit = result.ref
-   //   result.ref = ''
-   // }
-   // core.debug(`ref = '${result.ref}'`)
-   // core.debug(`commit = '${result.commit}'`)
+// Define 'repository'
+// result.repository = core.getInput('repository')
+// if (!result.repository) {
+//   if (isWorkflowRepository) {
+//     result.repository = github.context.repository
+//     result.commit = github.context.sha
+
+//     // Some events have an unqualifed repository. For example when a PR is merged (pull_request closed event),
+//     // the repository is unqualifed like "main" instead of "repositorys/heads/main".
+//     if (result.commit && result.repository && !result.repository.startsWith('repositorys/')) {
+//       result.repository = `repositorys/heads/${result.repository}`
+//     }
+//   }
+// }
+// // SHA?
+// else if (result.repository.match(/^[0-9a-fA-F]{40}$/)) {
+//   result.commit = result.repository
+//   result.repository = ''
+// }
+// core.debug(`repository = '${result.repository}'`)
+// core.debug(`commit = '${result.commit}'`)
 
 
-   // Auth token
+// Auth token
 //   result.authToken = core.getInput('token', {required: true})
 
-   // Destination on runner
-   //  process.env['RUNNER_TEMP']
+// Destination on runner
+//  process.env['RUNNER_TEMP']
 
-   // Download archive
+// Download archive
 
-   // Extract archive
+// Extract archive
 // }
 
 
 
-const loadDotenvFile = () => {
-   const fs = require('fs')
-   const dotenv = require('dotenv')
-   return dotenv.parse(fs.readFileSync('.env'))
+const loadDotenvFile = (filepath = '.env') => {
+   return dotenv.parse(
+      fs.readFileSync(filepath)
+   );
+};
 
+const cloneConfigTar = async (owner, repo, branch, token, destination) => {
+   // Making sure target path is accessible
+   await io.mkdirP(destination);
+
+   // Login with token
+   const octokit = github.getOctokit(token);
+
+   const params = {
+      owner: owner,
+      repo: repo,
+      ref: branch
+   };
+   core.info("Downloading tar archive");
+   core.debug(params);
+   const response = await octokit.repos.downloadTarballArchive(params);
+   if (response.status != 200) {
+      throw new Error(`Enable to fetch repository. HTTP:[${response.status}], content:[${response.data}]`);
+   }
+
+   const downloadUuid = uuidv4();
+   console.dir(downloadUuid);
+   const archiveFilepath = path.join(destination, `archive-${repo}-${downloadUuid}.tar.gz`);
+   core.info(`Writing archive file [${archiveFilepath}] to disk`);
+   const archiveData = Buffer.from(response.data);
+   await fs.promises.writeFile(archiveFilepath, archiveData);
+
+   // Extract archive
+   const repoPath = path.join(destination, `${repo}-${downloadUuid}`);
+   core.info(`Extracting archive to [${repoPath}]`);
+   await tc.extractTar(archiveFilepath, repoPath);
+
+   // Cleanup archive
+   await io.rmRF(archiveFilepath);
+
+   // Env content is in archives single folder
+   const archiveContent = await fs.promises.readdir(repoPath);
+   const dotenvConfigPath = path.resolve(archiveContent[0]); // The top-level folder name includes the short SHA
+   core.info(`Configuration available at [${dotenvConfigPath}]`);
+
+   return dotenvConfigPath;
+};
+
+const cloneConfigZip = async (owner, repo, branch, token, destination) => {
+   // Making sure target path is accessible
+   await io.mkdirP(destination);
+
+   // Login with token
+   const octokit = github.getOctokit(token);
+
+   const params = {
+      owner: owner,
+      repo: repo,
+      ref: branch
+   };
+   core.info("Downloading zip archive");
+   core.debug(params);
+   const response = await octokit.repos.downloadZipballArchive(params);
+   if (response.status != 200) {
+      throw new Error(`Enable to fetch repository. HTTP:[${response.status}], content:[${response.data}]`);
+   }
+
+   const downloadUuid = uuidv4();
+   console.dir(downloadUuid);
+   const archiveFilepath = path.join(destination, `archive-${repo}-${downloadUuid}.zip`);
+   core.info(`Writing archive file [${archiveFilepath}] to disk`);
+   const archiveData = Buffer.from(response.data);
+   await fs.promises.writeFile(archiveFilepath, archiveData);
+
+   // Extract archive
+   const repoPath = path.join(destination, `${repo}-${downloadUuid}`);
+   core.info(`Extracting archive to [${repoPath}]`);
+   await tc.extractZip(archiveFilepath, repoPath);
+
+   // Cleanup archive
+   await io.rmRF(archiveFilepath);
+
+   // Env content is in archives single folder
+   const archiveContent = await fs.promises.readdir(repoPath);
+   const dotenvConfigPath = path.resolve(archiveContent[0]); // The top-level folder name includes the short SHA
+   core.info(`Configuration available at [${dotenvConfigPath}]`);
+
+   return dotenvConfigPath;
 };
 
 const inputs = () => {
    return {
-      // The repository to fetch
-      ref: core.getInput('ref'),
+      // The repository to fetch (<owner>/<repo>)
+      repository: core.getInput('repository'),
+      owner: core.getInput('repository').split('/')[0],
+      repo: core.getInput('repository').split('/')[1],
+
+      // This should be a token with access to your repository scoped in as a secret
+      // token: ${{ secrets.GITHUB_TOKEN }}
+      token: core.getInput('token'),
 
       // The branch to checkout (default: main)
       branch: core.getInput('branch') || "main",
 
-      // Look for file in subdirectory
+      // The working folder to write configuration to (default '.')
+      destination: core.getInput('destination') || '.',
+
+      // Look for file in subdirectory (default '.')
       directory: core.getInput('directory') || '.',
 
-      // The config filename (default to application.yml)
-      filename: core.getInput('filename') || "application.yml",
+      // The config filename (default to 'main.env')
+      filename: core.getInput('filename') || "main.env",
 
       // profile for file (ex: 'prod' will make tool look for <filename_part>-<profile>.<filename_extension>)
       // extension represents the last dot of a filename (if any)
@@ -76,19 +174,23 @@ const inputs = () => {
    };
 }
 
-// most @actions toolkit packages have async methods
+// Most @actions toolkit packages have async methods
 // 'core.debug' displays only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    const settings = inputs();
-    core.info(`Waiting ${ms} milliseconds ...`);
-    core.debug(settings);
-    core.debug(loadDotenvFile());
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+   try {
+      const settings = inputs();
+      core.debug(settings);
+      core.debug(loadDotenvFile());
+      if (process.platform === 'win32') {
+         // Windows
+         cloneConfigZip(settings.owner, settings.repo, settings.branch, settings.token, settings.destination);
+      } else {
+         // Unix
+         cloneConfigTar(settings.owner, settings.repo, settings.branch, settings.token, settings.destination);
+      }
+   } catch (error) {
+      core.setFailed(error.message);
+   }
 }
 
 run();
